1927502cf23f364076f9ca0ba7074ca4f614ff04
Guairden <valentin.hoyet@gmail.com>
2020-11-18 11:34:05 +0100

    cleaning

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ svg.addEventListener('load', function () {
   const boxes = svgContent.getElementsByTagName('rect');
   const texts = svgContent.getElementsByTagName('text');
   const bridges = svgContent.getElementsByTagName('line');
+  addCss(svgRoot);
 
   for (box of boxes) {
     box.addEventListener('click', function (e) {
@@ -29,12 +30,6 @@ svg.addEventListener('load', function () {
       element.removeAttribute('focus');
     }
 
-    
-
-    
-    
-
-
     /*var text = document.createElementNS('ttp://www.w3.org/2000/svg', 'text');
     text.setAttributeNS('null', 'x', parseInt(element.getAttribute('x')) + 7);
     text.setAttributeNS('null', 'y', parseInt(element.getAttribute('y')) + 14);
@@ -50,123 +45,116 @@ svg.addEventListener('load', function () {
 
 }, false);
 
-function getIndexOf(htmlCollection, element) {
-  let i = 0;
-  for (e of htmlCollection) {
-    if (e == element)
-      return i;
-
-    i++;
-  }
-}
-
-function getContainer(svgContent, element) {
-  if(element.id.includes('Machine'))
-    return null;
-
-  return svgContent.getElementById(element.getAttribute('parent_id'));
-}
-
 function expandTopology(svgContent, svgRoot, boxes, texts, bridges, element, lag) {
-  svgRoot.setAttribute('height', parseInt(svgRoot.getAttribute('height')) + lag);
-  expandTopologyByElement(boxes, texts, bridges, element, lag);
-
+  expandElement(svgRoot, lag);
+  mooveElementsBelow(boxes, texts, bridges, element, lag);
+  expandElement(element, lag);
+  
   while(element = getContainer(svgContent, element)) {
-    if(isContainer(element))
-      expandTopologyByElement(boxes, texts, bridges, element, lag);
+    if(isContainer(element)) {
+      mooveElementsBelow(boxes, texts, bridges, element, lag);
+      expandElement(element, lag);
+    }
   }
 
-  function expandTopologyByElement(boxes, texts, bridges, element, lag) {
+  function mooveElementsBelow(boxes, texts, bridges, element, lag) {
     let x = element.getAttribute('x');
     let width = element.getAttribute('width');
     let y = parseInt(element.getAttribute('y')) + parseInt(element.getAttribute('height'));
-    
-    if(!element.getAttribute('expanded'))
-      element.setAttribute('height', parseInt(element.getAttribute('height')) + lag)
 
     for (box of boxes) {
-      if (!box.getAttribute('expanded') && isElementBelow(x, y, width, box)) {
-        box.setAttribute('y', parseInt(box.getAttribute('y')) + lag);
-        box.setAttribute('expanded', true);
+      if (!isMooved(box) && isElementBelow(x, y, width, box)) {
+        mooveElement(box, lag);
 
-        if(!box.id.includes('Machine') && !box.id.includes('Package') && !box.id.includes('Group') && isElementLarger(x, width, box)) {
+        if(isContainer(box))
+            mooveChildren(svgContent, boxes, box, lag)
+
+        if(isElementLarger(x, width, box)) {
           //??? Using only box here seems to cause a reference problem for some reasons.
-
           let tmpBox = box
-          box.setAttribute('y', parseInt(box.getAttribute('y')) - lag);
-          expandTopologyByElement(boxes, texts, bridges, box, lag);
-          tmpBox.setAttribute('y', parseInt(tmpBox.getAttribute('y')) + lag);
+          box.setAttribute("transform","translate(" + 0 + ", " + 0 + ")");
+          mooveElementsBelow(boxes, texts, bridges, box, lag);
+          tmpBox.setAttribute("transform","translate(" + 0 + ", " + lag + ")");
           mooveTextChild(svgContent, tmpBox, lag)
-          
-          if(tmpBox.id.includes('PCI')) {
-            for(childBox of boxes) {
-              if(childBox.getAttribute('parent_id') == tmpBox.getAttribute('id')) {
-                childBox.setAttribute('y', parseInt(childBox.getAttribute('y')) + lag);
-                childBox.setAttribute('expanded', true);
-                mooveTextChild(svgContent, childBox, lag)
-              }
-            }
-          }
-
         }
         
       }
     }
 
     for (text of texts) {
-      if (!text.getAttribute('expanded') && isElementBelow(x, y, width, text)) {
-        text.setAttribute('y', parseInt(text.getAttribute('y')) + lag);
-        text.setAttribute('expanded', true);
+      if (!isMooved(text) && isElementBelow(x, y, width, text)) {
+        mooveElement(text, lag);
       }
     }
 
     for (bridge of bridges) {
-      if (!bridge.getAttribute('expanded') && parseInt(bridge.getAttribute('x1')) >= parseInt(x) && parseInt(bridge.getAttribute('x1')) <= parseInt(x) + parseInt(width) && parseInt(bridge.getAttribute('y1')) > y) {
-        bridge.setAttribute('y1', parseInt(bridge.getAttribute('y1')) + lag);
-        bridge.setAttribute('y2', parseInt(bridge.getAttribute('y2')) + lag);
-        bridge.setAttribute('expanded', true);
+      if (!isMooved(bridge) && isLineBelow(x, y, width, bridge)) {
+        mooveElement(bridge, lag);
       }
     }
   }
 }
 
 function shortenTopology(svgContent, svgRoot, boxes, texts, bridges, element, lag) {
-  svgRoot.setAttribute('height', parseInt(svgRoot.getAttribute('height')) - lag);
-  element.setAttribute('height', parseInt(element.getAttribute('height')) - lag);
+  retractElement(svgRoot, lag);
+  retractElement(element, lag);
 
   while(element = getContainer(svgContent, element)) {
     if(isContainer(element)) {
-      element.setAttribute('height', parseInt(element.getAttribute('height')) - lag);
-    }
-     
-  }
-
-  for (box of boxes) {
-    if (box.getAttribute('expanded')) {
-      box.setAttribute('y', parseInt(box.getAttribute('y')) - lag);
-      box.removeAttribute('expanded');
+      retractElement(element, lag);
     }
   }
 
-  for (text of texts) {
-    if (text.getAttribute('expanded')) {
-      text.setAttribute('y', parseInt(text.getAttribute('y')) - lag);
-      text.removeAttribute('expanded');
-    }
-  }
-
-  for (bridge of bridges) {
-    if (bridge.getAttribute('expanded')) {
-      bridge.setAttribute('y1', parseInt(bridge.getAttribute('y1')) - lag);
-      bridge.setAttribute('y2', parseInt(bridge.getAttribute('y2')) - lag);
-      bridge.removeAttribute('expanded');
-    }
+  for(child of svgRoot.children) {
+    if (isMooved(child))
+      mooveElement(child, 0);
   }
 
 }
 
+function expandElement(element, lag) {
+  element.setAttribute('height', parseInt(element.getAttribute('height')) + lag);
+}
+
+function retractElement(element, lag) {
+  element.setAttribute('height', parseInt(element.getAttribute('height')) - lag);
+}
+
+function mooveElement(element, lag) {
+  element.setAttribute("transform","translate(" + 0 + ", " + lag + ")");
+  if(lag == 0)
+    element.removeAttribute('mooved');
+  else
+    element.setAttribute('mooved', true);
+}
+
+function mooveTextChild(svgContent, box, lag) {
+  // Can there be more than one ?
+  let boxText = svgContent.getElementById(box.id.replace('rect', 'text'));
+  if(boxText && !isMooved(boxText)) {
+    boxText.setAttribute("transform","translate(" + 0 + ", " + lag + ")");
+    boxText.setAttribute('mooved', true);
+  }
+}
+
+function mooveChildren(svgContent, boxes, element, lag) {
+  for(childBox of boxes) {
+    if(childBox.getAttribute('parent_id') == element.getAttribute('id')) {
+      childBox.setAttribute("transform","translate(" + 0 + ", " + lag + ")");;
+      childBox.setAttribute('mooved', true);
+      mooveTextChild(svgContent, childBox, lag)
+    }
+  }
+}
+
 function isElementBelow(x, y, width, box) {
   if (parseInt(box.getAttribute('y')) > y && ((parseInt(box.getAttribute('x')) < x && parseInt(box.getAttribute('x')) + parseInt(box.getAttribute('width')) > parseInt(x)) || (parseInt(box.getAttribute('x')) >= parseInt(x) && parseInt(box.getAttribute('x')) <= parseInt(x) + parseInt(width))))
+    return true;
+  return false;
+}
+
+function isLineBelow(x, y, width, bridge) {
+  if(parseInt(bridge.getAttribute('x1')) >= parseInt(x) && parseInt(bridge.getAttribute('x1')) <= parseInt(x) + parseInt(width) && parseInt(bridge.getAttribute('y1')) > y)
     return true;
   return false;
 }
@@ -186,17 +174,40 @@ function isTopologyFocused(boxes){
   return null;
 }
 
+function isMooved(element) {
+  if(element.getAttribute('mooved'))
+    return true;
+  return false;
+}
+
 function isContainer(element) {
   if((element.id.includes('Package') || element.id.includes('Group') || element.id.includes('Core') || element.id.includes('Bridge') || element.id.includes('PCI')) && !element.getAttribute('class').includes('Bridge'))
     return true;
   return false;
 }
 
-function mooveTextChild(svgContent, box, lag) {
-  // Can there be more than one ?
-  let boxText = svgContent.getElementById(box.id.replace('rect', 'text'));
-  if(boxText && !boxText.getAttribute('expanded')) {
-    boxText.setAttribute('y', parseInt(boxText.getAttribute('y')) + lag);
-    boxText.setAttribute('expanded', true);
+function getContainer(svgContent, element) {
+  if(element.id.includes('Machine'))
+    return null;
+
+  return svgContent.getElementById(element.getAttribute('parent_id'));
+}
+
+function getIndexOf(htmlCollection, element) {
+  let i = 0;
+  for (e of htmlCollection) {
+    if (e == element)
+      return i;
+
+    i++;
   }
+}
+
+function addCss(svgRoot) {
+  const link = document.createElement('link');
+  link.setAttribute('xmlns', 'http://www.w3.org/1999/xhtml');
+  link.setAttribute('rel', 'stylesheet');
+  link.setAttribute('href', '../css/style.css');
+  link.setAttribute('type', 'text/css');
+  svgRoot.append(link);
 }
