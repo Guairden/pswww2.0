6224bbf7194eafbd929648570e7277b866d91ff4
Guairden <valentin.hoyet@gmail.com>
2020-12-02 17:34:37 +0100

    get infos + xml and svg from xml

--- main.js
+++ main.js
@@ -1,43 +1,37 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
+const io = require('socket.io-client');
+const fs = require('fs');
+const socket = io('http://localhost:3000');
+let parseString = require('xml2js').parseString;
+let xml;
+
+socket.emit('get_svg', '');
+socket.on('set_svg', (msg) => {
+  fs.writeFile('./img/foo.svg', msg, function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(data);
+  });
+});
+
+socket.emit('get_xml', '');
+socket.on('set_xml', (msg) => {
+  console.log(msg)
+  parseString(msg, function (err, result) {
+    global.xml = JSON.parse(JSON.stringify(result));
+  });
+});
 
 function createWindow () {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      enableRemoteModule: true
     }
   })
-
-  win.webContents.setZoomFactor(1.0); 
-  
-  // Upper Limit is working of 500 % 
-  win.webContents 
-      .setVisualZoomLevelLimits(1, 5) 
-      .then(console.log("Zoom Levels Have been Set between 100% and 500%")) 
-      .catch((err) => console.log(err)); 
-    
-  win.webContents.on("zoom-changed", (event, zoomDirection) => { 
-      console.log(zoomDirection); 
-      var currentZoom = win.webContents.getZoomFactor(); 
-      console.log("Current Zoom Factor - ", currentZoom); 
-      console.log('Current Zoom Level at - ', win.webContents.getZoomLevel()); 
-    
-      if (zoomDirection === "in") { 
-          
-          win.webContents.setZoomFactor(currentZoom + 0.20);
-    
-          console.log("Zoom Factor Increased to - "
-                      , win.webContents.zoomFactor * 100, "%"); 
-      } 
-      if (zoomDirection === "out") { 
-          if(win.webContents.zoomFactor > 0.2)
-            win.webContents.setZoomFactor(currentZoom - 0.20);
-    
-          console.log("Zoom Factor Decreased to - "
-                      , win.webContents.zoomFactor * 100, "%"); 
-      } 
-  });
 
   win.loadFile('./html/index.html')
   win.webContents.openDevTools()
@@ -51,8 +45,6 @@ app.on('window-all-closed', () => {
   }
 })
 
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
-})
+ipcMain.on('get_xml', async (event, arg) => {
+  event.sender.send('set_xml', xml);
+});
